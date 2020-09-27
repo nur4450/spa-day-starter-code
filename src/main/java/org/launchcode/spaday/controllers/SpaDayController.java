3abfed2d6dde5d0e7b01190bc9e433fb3437ffbb
Nur Cetin <seymanur4450@gmail.com>
2020-09-27 16:07:03 -0500

    Finished Studio.

--- src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
+++ src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
@@ -22,6 +22,12 @@ public class SpaDayController {
         newClient.setAppropriateFacials(skintype);
         model.addAttribute("client" , newClient);
 
+        model.addAttribute("name",name);
+        model.addAttribute("skintype",skintype);
+        model.addAttribute("manipedi",manipedi);
+        model.addAttribute("appropriateFacials",appropriateFacials);
+        model.addAttribute("facials",facials);
+
         return "menu";
     }
 }
