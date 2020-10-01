013f7fcbd0c938f16aea884bdad25ac909454ba6
Nur Cetin <seymanur4450@gmail.com>
2020-09-30 19:35:39 -0500

    commit

--- src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
+++ src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
@@ -16,60 +16,12 @@ public class SpaDayController {
         return "serviceSelection";
     }
 
-<<<<<<< HEAD
-    @GetMapping(value="")
-    @ResponseBody
-    public String customerForm () {
-        String html = "<form method = 'post'>" +
-                "Name: <br>" +
-                "<input type = 'text' name = 'name'>" +
-                "<br>Skin type: <br>" +
-                "<select name = 'skintype'>" +
-                "<option value = 'oily'>Oily</option>" +
-                "<option value = 'combination'>Combination</option>" +
-                "<option value = 'normal'>Normal</option>" +
-                "<option value = 'dry'>Dry</option>" +
-                "</select><br>" +
-                "Manicure or Pedicure? <br>" +
-                "<select name = 'manipedi'>" +
-                "<option value = 'manicure'>Manicure</option>" +
-                "<option value = 'pedicure'>Pedicure</option>" +
-                "</select><br>" +
-                "<input type = 'submit' value = 'Submit'>" +
-                "</form>";
-        return html;
-    }
-
-    @PostMapping(value="")
-    public String spaMenu(@RequestParam String name, @RequestParam String skintype, @RequestParam String manipedi, Model model) {
-
-        ArrayList<String> facials = new ArrayList<String>();
-        facials.add("Microdermabrasion");
-        facials.add("Hydrofacial");
-        facials.add("Rejuvenating");
-        facials.add("Enzyme Peel");
-
-        ArrayList<String> appropriateFacials = new ArrayList<String>();
-        for (int i = 0; i < facials.size(); i ++) {
-            if (checkSkinType(skintype,facials.get(i))) {
-                appropriateFacials.add(facials.get(i));
-            }
-        }
-
-        model.addAttribute("name",name);
-        model.addAttribute("skintype",skintype);
-        model.addAttribute("manipedi",manipedi);
-        model.addAttribute("appropriateFacials",appropriateFacials);
-        model.addAttribute("facials",facials);
-
-=======
     @PostMapping
     public String processClientForm(@RequestParam String skintype, @RequestParam String manipedi, Model model) {
         Client newClient = new Client(skintype, manipedi);
         newClient.setAppropriateFacials(skintype);
         model.addAttribute("client" , newClient);
 
->>>>>>> 41bafc8e5f2053ddff4668047ba80efb619a4da2
         return "menu";
     }
 }
--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,9 +1,26 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank(message = "Username cannot be blank.")
+    @Size(min = 5, max = 15, message = "Username must be between 5 and 15 characters.")
     private String username;
+
+    @Email(message = "Invalid email format.")
     private String email;
+
+    @NotBlank(message = "Password cannot be blank.")
+    @Size(min = 6, message = "Password must be at least 6 character.")
     private String password;
+
+    @NotNull(message = "Password do not match.")
+    private String verify;
+
 
     public User() {
 
